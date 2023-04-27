f08c880e5e5e38cf80d31dd0ed3613f2f11da97f
Radu-A <elcastravete@gmail.com>
2023-04-27 22:12:48 +0200

    falta el bonus

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
+
+document.title = "Generasión 1 Pokimon";
 console.log(document.title);
+
+document.getElementById("gen-1").innerHTML= "Generasión 1 Pokimon";
+
+document.getElementsByClassName("infocard-list infocard-list-pkmn-lg")[0].id = "generacion1";
+
+document.getElementById("generacion1").style.backgroundColor = "aquamarine";
+
+console.log(document.URL);
+
+console.log(document.domain);
+
+const images = document.getElementsByTagName("img");
+console.log(images);
+
+for (let i = 0; i < images.length; i++) {
+    images[i].setAttribute("src", "https://media.giphy.com/media/2v170e71aanfi/giphy.gif");
+}
+
